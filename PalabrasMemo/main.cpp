40cdd4efd71828c512d3b216513960b423e48e57
emifervi <35278534+emifervi@users.noreply.github.com>
2018-01-29 14:31:36 -0600

    .

---
+++ PalabrasMemo/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream ArchTexto, ArchPalabras;
+
+    ArchPalabras.open("Palabras.txt");
+    string Palabras,Linea;
+
+    int i=0
+
+    while(!ArchPalabras.eof()){
+        ArchPalabras>>Palabras;
+        while(!ArchTexto>>Linea){
+
+            if(substr)
+        }
+    }
+
+
+    while()
+    return 0;
+}
