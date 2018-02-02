a010c906fab4b176f6639dfe5f0682e28b69460e
emifervi <35278534+emifervi@users.noreply.github.com>
2018-02-01 18:16:51 -0600

    Add files via upload

--- LibrarySystem/main.cpp
+++ LibrarySystem/main.cpp
@@ -19,31 +19,35 @@ int main(){
     char cOption;
 
     do{
-
     // Asks user for the info about the book and store in variables.
     cout << "+ What's the name of the author?\n";
+    cin.ignore();
     getline(cin, author);
     cout << "\n+ Which title is it?\n";
     cin >> title;
-    cout << "\n+ What's the price? ";
-    cin >> price;
+    do{
+        cout << "\n+ What's the price? ";
+        cin >> price;
+    }while (price <= 0);
 
     // Declare object of class book and assign values to the attributes.
     Book regBook(author, title, price);
 
     // Display the info given by the user about the book.
-    regBook.Display();
+    regBook.display();
 
     // Display the current price and ask which percent of that price do they want to raise the cost of the book.
-    cout << "\n+ Current price: " << regBook.getPrice() << "\nWhich percent do you want to raise the price ? ";
-    cin >> percentage;
+    do{
+        cout << "\n+ Current price: " << regBook.getPrice() << "\nWhich percent do you want to raise the price ? ";
+        cin >> percentage;
+    }while(percentage <= 0);
 
     // Call raisePrice method of the class Book.
     regBook.raisePrice(percentage);
 
     // Display new data about the book.
     cout << "\n+ New data about the book: \n";
-    regBook.Display();
+    regBook.display();
 
     // Ask if the user wants to go at it again with another book.
     cout << "\n+ Do you want to do another one? (y/n).\n";
